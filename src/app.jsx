03e580c75e8eff9a7ad7f0b548285e1aad918968
Eduardo Molinar <edy_0440@hotmail.com>
2018-04-14 12:26:02 -0500

    deleted stub data

--- src/app.jsx
+++ src/app.jsx
@@ -12,30 +12,7 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-  description: "Water bill",
-  amount: 5000,
-  note: "have to pay it or will get cut off"
-}));
-
-store.dispatch(addExpense({
-  description: "Gas bill",
-  amount: 300,
-  note: "have to pay it or will also get cut off",
-  createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-  description: "Rent",
-  amount: 109500,
-  note: "have to pay it or will get cut off"
-}));
-
 const state = store.getState();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
